2715bcc214c8d72a6b15bd549c5fcd1caee65c9a
Cortland Goffena <cortland.goffena@hotmail.com>
2021-01-10 17:59:32 -0600

    modifying

--- Obelisk/mint/accounts.py
+++ Obelisk/mint/accounts.py
@@ -1,10 +1,6 @@
 ACCOUNTS = {
-    "CHOICE MONEY MARKET SAVINGS": 8394662, #AccountName to AccountID
-    "Main Checking": 8394663,
-    "Roth IRA-1": 8979274,
-    "Single-1": 8979273,
-    "CashBack CREDIT CARD": 8884438,
-    "MileagePlus CREDIT CARD": 8884437,
-    "CLASSIC SAVINGS": 8943747,
-    "Toyota RAV4": 8943755
+    "Account_1": 8394662, #AccountName to AccountID
+    "Account_2": 8394663,
+    "Account_3": 8979274,
+    "Account_4": 8979273
 }
